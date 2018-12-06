867195385db97499d74e82e224679608a903df7b
Sergei Portsev <Sergei_Portsev@epam.com>
2018-12-06 09:58:04 +0400

    config ts and config jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "preset": "react-native",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js"
+    ],
+    "transform": {
+        "^.+\\.(js)$": "<rootDir>/node_modules/babel-jest",
+        "\\.(ts|tsx)$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+    },
+    "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+    "testPathIgnorePatterns": [
+        "\\.snap$",
+        "<rootDir>/node_modules/"
+    ],
+    "cacheDirectory": ".jest/cache"
+}
--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Azaza</Text>
       </View>
     );
   }
